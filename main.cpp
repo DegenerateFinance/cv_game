e9b6e861a86803ceea7691da8173a38f887facee
RchOne <wlricardo1@gmail.com>
2022-04-28 12:33:00 +0200

    we managed to draw a rectangle in the camera, and flip the camera

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
-#include <opencv2/highgui.hpp>  //esto se supone q incluye las librerías
 #include <iostream>
+#include <opencv2/core/core.hpp>
+
+// Drawing shapes
+#include <opencv2/imgproc.hpp>
+
+#include <opencv2/highgui/highgui.hpp>
 // makefile con esto  g++ test.cpp -o testoutput -std=c++11 `pkg-config --cflags --libs opencv`
 
 using namespace std;
@@ -36,8 +41,34 @@ int main( int argc, char** argv )
                 return 1;
             }
 
-            imshow("testframe", frame);
 
+            flip(frame, frame, 1);      //this flip the img so it is more intuitive for the user
+            //we flip first so we don't have to flip the other data, like the squares shown
+
+
+
+            Point start_point(5, 5);
+
+            //Ending coordinate, here (220, 220)
+            //represents the bottom right corner of rectangle
+            Point end_point(220, 220);
+
+            //Blue color in BGR
+
+            //Line thickness of 2 px
+            int thickness = 2;
+
+            //Using cv2.rectangle() method
+            //Draw a rectangle with blue line borders of thickness of 2 px
+            rectangle(frame, start_point, end_point,
+                          Scalar(0, 255, 255),
+                          thickness, 8, 0);
+
+
+
+
+
+            imshow("testframe", frame);
             pressedKey=waitKey(1);
         }
         destroyWindow("testvid");
